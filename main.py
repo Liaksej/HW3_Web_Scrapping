d4c1c1823a6bbf9201db64c1e577a9c1bf13e964
Liaksej <tarusov.liaksej@gmail.com>
2022-09-03 20:50:01 +0200

    Star version 1

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Scrapping_habr:
         ).generate()
         return header
 
-    def get_all_articles_from_page(self):
+    def get_all_articles_from_preview(self):
         headers = self.get_headers()
         response = requests.get(url=(self.url + '/ru/all/'), headers=headers)
         text = response.text
@@ -25,13 +25,22 @@ class Scrapping_habr:
         articles = soup.find_all('article')
         return articles
 
+    def get_text_of_the_article(self, link_of_article):
+        headers = self.get_headers()
+        response = requests.get(url=link_of_article, headers=headers)
+        text = response.text
+        soup = BeautifulSoup(text, features='html.parser')
+        text_article = soup.find_all(class_="article-formatted-body article-formatted-body article-formatted-body_version-2")[0].text
+        return text_article
+
     def find_articles_with_keywords(self, keywords):
-        articles = self.get_all_articles_from_page()
+        articles = self.get_all_articles_from_preview()
         for article in articles:
-            text_preview = article.find_all(class_="article-formatted-body "
-                                                   "article-formatted-body article-formatted-body_version-2")
-            if len(text_preview) != 0:
-                text_for_analys = text_preview[0].text.replace('.', ' ').lower().split(' ')
+            link_of_article = self.url + article.find(class_="tm-article-"
+                                                             "snippet").contents[2].next.attrs['href']
+            print(link_of_article)
+            if len(link_of_article) != 0:
+                text_for_analys = self.get_text_of_the_article(link_of_article).replace('.', ' ').lower().split(' ')
                 for keyword in keywords:
                     if keyword in text_for_analys:
                         date_of_article = article.find(class_="tm-article-snippet_"
@@ -40,6 +49,9 @@ class Scrapping_habr:
                         link_of_article = self.url + article.find(class_="tm-article-"
                                                                          "snippet").contents[2].next.attrs['href']
                         print(f'{date_of_article} - {header_of_article} - {link_of_article}')
+                    else:
+                        self.get_text_of_the_article(link_of_article)
+
 
 
 if __name__ == '__main__':
